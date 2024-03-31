9e55f315289cc4b4ccd5215807f0b6d51a5b07c8
ChunchoMetalero <juan.valdivia.g@ug.uchile.cl>
2024-03-31 05:41:27 -0300

    Comienzo ver-productos

---
+++ Tarea 1/app/js/ver-productos.js
@@ -0,0 +1,11 @@
+fetch("../listado-productos.json")
+    .then((response) => response.json())
+    .then((data) => {
+        const productos = data.productos;
+        const listaProductos = document.querySelector("#lista-productos");
+        productos.forEach((producto) => {
+            const td = document.createElement("td");
+            td.textContent = producto.nombre;
+            listaProductos.appendChild(td);
+        });
+    });
