33fb25c7f288b361cb9a2cf88ca7d053cd7af20b
ChunchoMetalero <juan.valdivia.g@ug.uchile.cl>
2024-04-01 05:09:34 -0300

    Pequeños cambios

--- Tarea 1/app/js/ver-productos.js
+++ Tarea 1/app/js/ver-productos.js
@@ -2,7 +2,7 @@ fetch("../listado-productos.json")
     .then((response) => response.json())
     .then((data) => {
         const productos = data.productos;
-        const listaProductos = document.querySelector("#lista-productos");
+        const listaProductos = document.getElementById("lista-productos");
         productos.forEach((producto) => {
             const td = document.createElement("td");
             td.textContent = producto.nombre;
