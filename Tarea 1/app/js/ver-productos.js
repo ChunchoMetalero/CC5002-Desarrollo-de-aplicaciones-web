70c26e5c94a2a1e3cd28b2adfaf76f8c0d6ba4ed
ChunchoMetalero <juan.valdivia.g@ug.uchile.cl>
2024-04-04 02:15:09 -0300

    Fotos añadidas

--- Tarea 1/app/js/ver-productos.js
+++ Tarea 1/app/js/ver-productos.js
@@ -1,11 +1,23 @@
-fetch("../listado-productos.json")
-    .then((response) => response.json())
-    .then((data) => {
-        const productos = data.productos;
-        const listaProductos = document.getElementById("lista-productos");
-        productos.forEach((producto) => {
-            const td = document.createElement("td");
-            td.textContent = producto.nombre;
-            listaProductos.appendChild(td);
+var tbody = document.getElementById("lista-productos");
+
+// Realizar una solicitud HTTP GET para obtener el archivo JSON
+fetch("../data/listado-productos.json")
+    .then(response => response.json())
+    .then(data =>{
+        // Recorrer los datos y agregar filas a la tabla
+        data.forEach(function(producto) {
+            var fila = "<tr>";
+            fila += "<td>" + producto.tipo + "</td>";
+            fila += "<td>" + producto.productos.join(", ") + "</td>";
+            fila += "<td>" + producto.region + "</td>";
+            fila += "<td>" + producto.comuna + "</td>";
+            fila += "<td><img src='" + producto.imagen + "' alt='Product Image' width='120' height='120'></td>";
+            fila += "</tr>";
+
+            // Agregar la fila al tbody de la tabla
+            tbody.innerHTML += fila;
         });
+    })
+    .catch(function(error) {
+        console.error('Error fetching the JSON file:', error);
     });
