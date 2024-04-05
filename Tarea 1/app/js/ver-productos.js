6a4a255666b522d5ae60114abe11b710c94e37f0
ChunchoMetalero <juan.valdivia.g@ug.uchile.cl>
2024-04-05 04:14:27 -0300

    informacion producto completa

--- Tarea 1/app/js/ver-productos.js
+++ Tarea 1/app/js/ver-productos.js
@@ -6,7 +6,7 @@ fetch("../data/listado-productos.json")
     .then(data =>{
         // Recorrer los datos y agregar filas a la tabla
         data.forEach(function(producto) {
-            var fila = "<tr>";
+            var fila = "<tr onClick='mostrarInformacionProducto(" + producto.id + ")'>";
             fila += "<td>" + producto.tipo + "</td>";
             fila += "<td>" + producto.productos.join(", ") + "</td>";
             fila += "<td>" + producto.region + "</td>";
@@ -21,3 +21,24 @@ fetch("../data/listado-productos.json")
     .catch(function(error) {
         console.error('Error fetching the JSON file:', error);
     });
+
+
+function mostrarInformacionProducto(productId) {
+    console.log("Esto funciona");
+    console.log("Producto seleccionado: " + productId);
+    // Obtener los datos del producto correspondiente al ID
+    fetch("../data/listado-productos.json")
+        .then(response => response.json())
+        .then(data => {
+            // Buscar el producto en el arreglo de productos
+            var producto = data.find(producto => producto.id == productId);
+            // Guardar los datos del producto en el localStorage
+            localStorage.setItem("producto", JSON.stringify(producto));
+            // Redirigir a la página de información del producto
+            window.location.href = "informacion-producto.html";
+        })
+        .catch(function(error) {
+            console.error('Error fetching the JSON file:', error);
+        });
+}
+    
